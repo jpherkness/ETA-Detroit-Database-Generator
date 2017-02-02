ac721bf1bb8d13ed7ea9f065e1a9c005e94bce1c
Nagasurendra <nagasurendra.kumar@gmail.com>
2017-02-02 12:03:53 -0500

    Load RefLex Data into Firebase

--- runme.py
+++ runme.py
@@ -11,15 +11,15 @@ def main():
     routes, stop_orders, stop_locations = [], [], []
 
     database.setupDatabase()
-    smart_routes, smart_stop_orders, smart_stop_locations = smart.load_data()
+    reflex_routes, reflex_stop_orders, reflex_stop_locations = reflex.load_data()
     #ddot.load_ddot_data()
     #reflex.load_reflex_data()
 
     fb.empty_firebase_database()
     
-    fb.insert_routes(smart_routes)
-    fb.insert_stop_orders(smart_stop_orders)
-    fb.insert_stop_locations(smart_stop_locations)
+    fb.insert_routes(reflex_routes)
+    fb.insert_stop_orders(reflex_stop_orders)
+    fb.insert_stop_locations(reflex_stop_locations)
 
     print("Database saved to", current_path + "/ETADetroitDatabase.db")
 
